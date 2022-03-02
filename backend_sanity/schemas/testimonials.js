8158a323bd4aff5c17ff4d019934f43eb7b78822
unknown <ndakunene@outlook.com>
2022-03-02 12:05:52 +0200

    update 3

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,11 +1,30 @@
-export default{
-    name:'testimonials',
-    title:'Testimonials',
-    TYPE:'document',
-    fields:[{
-        name:'',
-        title:'',
-        imageurl:'',
-        type:''
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    TYPE: 'document',
+    fields: [{
+        name: 'name',
+        title: 'Name',
+        type: 'string'
+    },
+    {
+        name: 'company',
+        title: 'Company',
+        type: 'string'
+    },
+    {
+        name: 'imageurl',
+        title: 'imgURL',
+        type: 'image',
+        option: {
+            hotspot: true  //allows you to crop the image
+        }
+    },
+    {
+        name: 'feedback',
+        title: 'Feedback',
+        type: 'string'
     }]
 }
+
+
