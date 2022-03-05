ebe1f46e36482ff5dd96a95fa36fcdfe211ebda5
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-05 15:05:14 +0200

    connected the frontend to sanity (about section)

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,42 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
+
 import './About.scss';
+import { urlFor, client } from '../../client';
 
 const abouts = [
   {
     title: 'Web Development',
     description: 'I am a good developer',
-    imgUrl: ''
+    imgUrl: images.about01
   },
   {
     title: 'Web Design',
     description: 'I am a good developer',
-    imgUrl: ''
+    imgUrl: images.about02
   },
   {
     title: 'UI/UX',
     description: 'I am a good developer',
-    imgUrl: ''
+    imgUrl: images.about03
   },
   {
-    title: 'Web Animation',
+    title: 'MERN Stack',
     description: 'I am a good developer',
-    imgUrl: ''
+    imgUrl: images.about04
   }];
 
 
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, [])
+
+
   return (
     <>
-      <h2 className='head-text'>
-        I kNow That
-        <span>Good design</span>
-        <br />
-        means
-        <span>Good Business</span>
-      </h2>
+      <h2 className='head-text'>I know that <span>Good Dev</span><br /> means <span>Good Business</span></h2>
 
       <div className='app__profiles'>
         {abouts.map((about, index) => (
