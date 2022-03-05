c5e63d03889808d04b0dd0f523c1bf4ae07aa09f
Ndabenhle Kunene <ndakunene@outlook.com>
2022-03-05 10:36:50 +0200

    update

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,6 +3,29 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 
+const abouts = [
+  {
+    title: 'Web Development',
+    description: 'I am a good developer',
+    imgUrl: ''
+  },
+  {
+    title: 'Web Design',
+    description: 'I am a good developer',
+    imgUrl: ''
+  },
+  {
+    title: 'UI/UX',
+    description: 'I am a good developer',
+    imgUrl: ''
+  },
+  {
+    title: 'Web Animation',
+    description: 'I am a good developer',
+    imgUrl: ''
+  }];
+
+
 
 const About = () => {
   return (
@@ -14,6 +37,17 @@ const About = () => {
         means
         <span>Good Business</span>
       </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+      <motion.div
+      
+      >
+
+      </motion.div>
+    ))}
+
+      </div>
     </>
   )
 }
