d3e8396a564efa1ff2c539875cb0c47a47d9829d
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-06 08:15:32 +0200

    6 march update 1

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -6,30 +6,6 @@ import { images } from '../../constants';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good developer',
-    imgUrl: images.about01
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good developer',
-    imgUrl: images.about02
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am a good developer',
-    imgUrl: images.about03
-  },
-  {
-    title: 'MERN Stack',
-    description: 'I am a good developer',
-    imgUrl: images.about04
-  }];
-
-
-
 const About = () => {
 
   const [abouts, setAbouts] = useState([]);
@@ -55,7 +31,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { motion } from 'framer-motion';
 
+import {AppWrap} from '../../wrapper';
+
 import { images } from '../../constants';
 import './Header.scss';
 
