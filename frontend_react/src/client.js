388a0a685ae1da57776f7d7bf9f366ab51b4676d
Ndabenhle Kunene <ndakunene@outlook.com>
2022-03-08 11:11:47 +0200

    update

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -7,7 +7,7 @@ export const client = sanityClient({
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    // token: process.env.REACT_APP_SANITY_TOKEN,
 })
 
 //Code for working with images in sanity
