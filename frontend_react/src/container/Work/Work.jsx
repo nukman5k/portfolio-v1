cd1158ac982434996130f7109c1dee3c61ee3ae7
Ndabenhle Kunene <ndakunene@outlook.com>
2022-03-08 09:19:05 +0200

    8March2022Update1

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -53,10 +53,41 @@ const Work = () => {
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeOut', staggerChildren: 0.5 }} //stagger means elements will show one after the other
+                transition={{ duration: 0.25, ease: 'easeOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex" //stagger means elements will show one after the other
               >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
 
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
               </motion.div>
+            </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p=text' style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className='app_work-tag app__flex'>
+                <p className='p-text'>{work.tags}</p>
+              </div>
             </div>
           </div>
         ))}
