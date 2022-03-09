e47fba4497b598f220eae0b27dbe1d6e057d2d27
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-09 20:45:04 +0200

    motionWrap updated

--- frontend_react/src/wrapper/MotionWrap.js
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -3,8 +3,14 @@ import { motion } from 'framer-motion';
 
 const MotionWrap = (Component, className) => function HOC() {
     return (
-        <div>NDA</div>
-    )
+        <motion.div
+            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+            transition={{ duration: 0.5 }}
+            className={`${classNames} app__flex`}
+        >
+            <Component />
+        </motion.div>
+    );
 }
 
 export default MotionWrap;
