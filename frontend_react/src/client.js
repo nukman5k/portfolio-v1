89c82c1a350ec004db85083728b6afe18c39f179
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-12 11:48:20 +0200

    update

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,15 +2,18 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    // projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    projectId: 'k58858es',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN
+    token: process.env.REACT_APP_SANITY_TOKEN,
+
 })
 
 //Code for working with images in sanity
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
+
+// projectId: 'k58858es',
+// token: "skW9JiPKAhjzgqdkgU0PKRrPpQiEfgajklKMxeRwpPQcIZlYtltwC0O9PgvPsRNUlMYJdDxcMkMZecegK1dgCoEyeRWsVpGZvM3KXh1RDbp6Si1UhcedYAPW4iaqccdxcxPDQCOVymP8JSWI3kO1GrMWwfryFjPG3H9n84NfFS1re3tx2qng"
